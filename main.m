f8a762d18580704a87469543538c32fbb37e72c7
ralireza <alirezarezaie0101@gmail.com>
2019-12-21 11:39:19 +0330

    add comment to main

--- main.m
+++ main.m
@@ -1,17 +1,28 @@
+% geting the feature of .bag file 
 [wa,a1,a2,wa_test,a1_test,a2_test]=get_all_feature();
 
-X = [wa(:,1:10);a1(:,1:10)];
-Y = [wa(:,11);a1(:,11)];
-target=[wa_test;a1_test];
+% train data
+X = [wa(:,1:10);a2(:,1:10)];
+% labels
+Y = [wa(:,11);a2(:,11)];
+% test data
+target=[wa_test;a2_test];
 
+% all classifiers implemented
 classifiers=["knn","rf","ab","nb","lda","lr","cart"]
 result = [];
+% table for show
 VarNames = {'classifier','accuracy', 'precision', 'recall', 'Fscore'};
 
+%  loop on classifier name
 for n = 1 : length(classifiers)
+%  pass the data and classifier name to function
     mdl=classifier( X,Y,classifiers(n));
+%     predicted data
     predicted = predict(mdl,target(:,1:10));
+%     evaluate data by function
     e=eval(target(:,11),predicted(:,1));
+%     showing result as a table
     T = table(classifiers(n),e.accuracy(1,1),e.precision(1,1),e.recall(1,1),e.Fscore(1,1), 'VariableNames',VarNames);
     result=[result;T];
 end
